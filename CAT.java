26fe45b86f44f22c28dc37a3569cff2dd3cc49eb
Prabhani_Jeewanthika <156695195+Prabhanijeewanthika02@users.noreply.github.com>
2024-06-05 17:08:50 +0530

    Update Declaration and Initialization Separately  CAT.java

--- CAT.java
+++ CAT.java
@@ -16,6 +16,11 @@ package com.mycompany.cat;
     public static void main(String[] args) {     // execute the program from Main method 
         
         CAT myCat1 = new CAT(); // Create an object of Cat class
+
+        //Update Declaration and Initialization Separately 
+
+        //CAT myCat1; Declaration: declaring a variable of type CAT
+        // myCat1 = new CAT(); Initialization: creating a new CAT object and assigning it to myCat1
   
         myCat1.eat();   // Calling the eat method
     }
