6f5f6467441e942e5a3e475a5153f90defd38c78
Prabhani_Jeewanthika <156695195+Prabhanijeewanthika02@users.noreply.github.com>
2024-06-08 13:25:44 +0530

    Added clarification of Java is not 100% object-oriented.

---
+++ java2.java
@@ -0,0 +1,9 @@
+public class java2 {
+    public static void main(String[] args) {
+        int primitiveInt = 5; // Primitive type
+        Integer objectInt = 5; // Wrapper class (Object)
+
+        System.out.println(primitiveInt); // Using primitive int
+        System.out.println(objectInt.toString()); // Using Integer object
+    }
+}
