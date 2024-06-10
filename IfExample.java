69b9dd9cfc604de7b00db3ca31350d072ca110a5
Prabhani_Jeewanthika <156695195+Prabhanijeewanthika02@users.noreply.github.com>
2024-06-11 01:32:12 +0530

    Add IfExample demonstrating basic if statement in Java

---
+++ IfExample.java
@@ -0,0 +1,10 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // if statement
+        if (number > 5) {
+            System.out.println("The number is greater than 5.");
+        }
+    }
+}
